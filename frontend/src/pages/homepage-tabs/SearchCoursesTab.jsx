cd39d64bf34955a9d3be0a4910a2f7c5986c753e
Aditya Cahyo <57148744+aditydcp@users.noreply.github.com>
2021-11-23 17:40:05 +0700

    implement AddCourseModal

--- frontend/src/pages/homepage-tabs/SearchCoursesTab.jsx
+++ frontend/src/pages/homepage-tabs/SearchCoursesTab.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import Button from "../../components/Button"
 import Card from "../../components/Card"
 import CourseCard from "../../components/CourseCard"
 import FloatingActionButton from "../../components/FloatingActionButton"
 import TabContainer from "./TabContainer"
 import Icon from "../../components/Icon"
+import AddCourseModal from "../../modals/AddCourseModal";
 
 const SearchCoursesTab = ({ ...props }) => {
+    const [modalVisible, setModalVisible] = useState(false)
+
+    const openModal = () => {
+        setModalVisible(true)
+    }
+
     return (
         <div className="w-full h-full flex flex-col items-center" {...props}>
+            <AddCourseModal visible={modalVisible} setVisible={setModalVisible} />
             <p className="mb-8 text-3xl mt-1/4-screen">Find a course!</p>
             <input placeholder="Search courses" type="text" className="border-2 rounded-md p-2 mb-8 w-1/2" />
             <div className="grid grid-cols-3 gap-4 px-16 w-full">
@@ -20,7 +29,7 @@ const SearchCoursesTab = ({ ...props }) => {
                 <CourseCard />
                 <CourseCard />
             </div>
-            <FloatingActionButton className="absolute bottom-8 right-16">
+            <FloatingActionButton className="absolute bottom-8 right-16" onClick={openModal}>
                 <Icon iconCode="icon-plus" className="text-white text-3xl" />
             </FloatingActionButton>
         </div>
